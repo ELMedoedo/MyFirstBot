e684ce195c30e511444e841e77f263ac01342c95
ElMedoedo <ZebraSkill@yandex.ru>
2025-03-04 18:57:02 +0300

    Починено

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ def main():
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
-    logging.info("Бот стартовал")
+    logging.info("Бот стартовал- успешно")
     mybot.start_polling()
     mybot.idle()
 
