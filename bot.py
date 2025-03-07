55f5aeac58a829f7e098274008356fbae8dcd6e3
ElMedoedo <ZebraSkill@yandex.ru>
2025-03-08 00:57:55 +0300

    Бот коррекция 1.4

--- bot.py
+++ bot.py
@@ -11,9 +11,13 @@ def greet_user(update, context):
     print("Вызван /start")
     update.message.reply_text("Привет, Боба! Зачем вызвал?")
 
-def name_planet(update, context):  
-    print('Вызван /planet')
-    update.message.reply_text('Введите название планеты на английском - Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto')
+
+def name_planet(update, context):
+    print("Вызван /planet")
+    update.message.reply_text(
+        "Введите название планеты на английском - Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto"
+    )
+
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
@@ -22,32 +26,33 @@ def main():
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("planet", name_planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    
 
     logging.info("Бот стартовал")
     mybot.start_polling()
     mybot.idle()
 
+
 def talk_to_me(update, context):
-    user_text=update.message.text
+    user_text = update.message.text
     print(user_text)
-    update.message.reply_text(user_text)
 
-    planets = {"Mercury": ephem.Mercury("2025/03/07"), 
-                "Venus": ephem.Venus("2025/03/07"), 
-                "Mars" : ephem.Mars("2025/03/07"), 
-                "Saturn": ephem.Saturn("2025/03/07"), 
-                "Uranus": ephem.Uranus("2025/03/07"), 
-                "Neptune": ephem.Neptune("2025/03/07"),
-                "Pluto": ephem.Pluto("2025/03/07")
-                }
-    if user_text in planets:
-        planet_input = user_text.lower().copitalize()
-        if planet_input in planets:
-            res=ephem.constellation(planets[planet_input])
-            update.message.reply_text(f"Планета: {planet_input}")
-            update.messege.reply_text(f"Сегодня 07-03-2025{user_text}")
-
+    planets = {
+        "Mercury": ephem.Mercury("2025/03/08"),
+        "Venus": ephem.Venus("2025/03/08"),
+        "Mars": ephem.Mars("2025/03/08"),
+        "Saturn": ephem.Saturn("2025/03/08"),
+        "Uranus": ephem.Uranus("2025/03/08"),
+        "Neptune": ephem.Neptune("2025/03/08"),
+        "Pluto": ephem.Pluto("2025/03/08"),
+    }
+    if user_text.lower().capitalize() in planets:
+        res = ephem.constellation(planets[user_text])
+        # update.message.reply_text(f"Названа планета планета: {user_text}")
+        # update.messege.reply_text(f"Сегодня 08-03-2025 {user_text}")
+        update.message.reply_text(f"Сегодня 08.03.2025 в созвездии: {res[1]}")
+        print(res)
+    else:
+        update.message.reply_text(f"Сам ты - {user_text}")
 
 
 if __name__ == "__main__":
