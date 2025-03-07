071389221ce918050cfcaa17be3e9496a61febba
ElMedoedo <ZebraSkill@yandex.ru>
2025-03-07 17:47:15 +0300

    Создание функции по планетам

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
+import ephem
 
 
 logging.basicConfig(filename="bot.log", level=logging.INFO)
@@ -10,13 +11,18 @@ def greet_user(update, context):
     print("Вызван /start")
     update.message.reply_text("Привет, Боба! Зачем вызвал?")
 
+def name_planet(update, context):  
+    print('Вызван /planet')
+    update.message.reply_text('Введите название планеты на английском - Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto')
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", name_planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    
 
     logging.info("Бот стартовал")
     mybot.start_polling()
@@ -27,4 +33,17 @@ def talk_to_me(update, context):
     print(user_text)
     update.message.reply_text(user_text)
 
-main()
+    planets = {"Mercury": ephem.Mercury("2025/03/07"), 
+                "Venus": ephem.Venus("2025/03/07"), 
+                "Mars" : ephem.Mars("2025/03/07"), 
+                "Saturn": ephem.Saturn("2025/03/07"), 
+                "Uranus": ephem.Uranus("2025/03/07"), 
+                "Neptune": ephem.Neptune("2025/03/07"),
+                "Pluto": ephem.Pluto("2025/03/07")
+                }
+    
+
+
+
+if __name__ == "__main__":
+    main()
